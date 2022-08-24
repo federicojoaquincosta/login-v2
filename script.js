0e17525ab966536291a2bbd36203fe3e439faf63
Federico Costa <federicojoaquincosta@gmail.com>
2022-08-24 18:16:50 -0300

    v1.0 funcional

--- script.js
+++ script.js
@@ -2,20 +2,54 @@ const USUARIOS = ["pepe", "jose", "carlos"]
 
 const CONTRASENIAS = ["123", "tateti", "hola!"]
 
-/* TODO MAL DESDE ACA EKISDE
 function verificar() {
     let us = document.querySelector("#usuario").value;
     let pw = document.querySelector("#contrasenia").value;
 
-    for (i = 0; i < USUARIOS.length; i++) {
-        if ((us = USUARIOS[i]) && (pw = CONTRASENIAS[i])) {
-            document.querySelector("#fracaso").innerHTML = ``
-            document.querySelector("#exito").innerHTML = `<h3> Un exito </h3>`;
-            break
-        } else {
-            document.querySelector("#fracaso").innerHTML = `<h3> UN FRACASO </h3>`
-        }
+    function checkuser(un) {
+        return un == us;
+    }
+    indexUN = USUARIOS.findIndex(checkuser);
+
+    function checkpw(pass) {
+        return pass == pw;
+    }
+    indexPW = CONTRASENIAS.findIndex(checkpw);
+
+    if ((indexUN > -1) && (indexPW == indexUN)) {
+        document.querySelector("#fracaso").innerHTML = ``;
+        document.querySelector("#exito").innerHTML = `Login exitoso`;
+
+    } else {
+        document.querySelector("#exito").innerHTML = ``;
+        document.querySelector("#fracaso").innerHTML = `Usuario y/o contraseña inválidos`;
     }
 
-    
+} 
+
+function registrar() {
+   let newuser = document.querySelector("#usuarionuevo").value;
+   let newpw = document.querySelector("#contrasenianueva1").value;
+   let pwcheck = document.querySelector("#contrasenianueva2").value;
+
+   function checkNewUser(nun) {
+    return nun == newuser;
+   }
+
+   indexNUN = USUARIOS.findIndex(checkNewUser);
+
+   if (indexNUN > -1) {
+    document.querySelector("#mensaje").innerHTML = `Ese nombre de usuario ya existe, por favor elija otro`
+   } else if (indexNUN = -1 && newpw != pwcheck) {
+    document.querySelector("#mensaje").innerHTML = `Por favor introduzca dos veces la misma contraseña`
+   } else if (newuser !== null && newpw !== null && pwcheck !== null) {
+    USUARIOS.push(document.querySelector("#usuarionuevo").value);
+    CONTRASENIAS.push(document.querySelector("#contrasenianueva1").value);
+    document.querySelector("#mensaje").innerHTML = `Usuario registrado exitosamente`
+   }
+}
+
+function mostrar(){
+    document.getElementById("botregis").style.display = "none"
+    document.getElementById("regocul").style.display = "block"
 }
